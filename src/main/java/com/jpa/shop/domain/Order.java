89cac8af687ac85b6de02376ec0d814c60e61f1e
Ohyaelim <npr05080@gmail.com>
2021-10-06 20:58:03 +0900

    feat: cascade 어노테이션 추가, 연관관계 편의 메서드 추가

--- src/main/java/com/jpa/shop/domain/Order.java
+++ src/main/java/com/jpa/shop/domain/Order.java
@@ -21,10 +21,14 @@ public class Order {
     @JoinColumn(name = "member_id") // 조인을 무엇으로 할 것인가 fk이름, 연관 관계의 주인
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // cascade, order의 orderItems에 데이터를 넣어두고 오더를 저장하면 오더 아이템도 저장된다.
     private List<OrderItem> orderItems = new ArrayList<>();
+    // entity당 각각 persist해줘야해서
+    // persist(orderItemA) persist(orderItemB) persist(order) 해야하지만
+    // cascade힘으로 persist(order)만하면된다. persist를 전파하는 역할
+    // all 옵션이니까 delete 할때도 다같이 삭제된다.
 
-    @OneToOne(fetch = FetchType.LAZY)  // 원투원일땐 주로 엑세스를 많이하는 쪽이 연관 관계의 주인이 되어야한다.
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)  // 원투원일땐 주로 엑세스를 많이하는 쪽이 연관 관계의 주인이 되어야한다.
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,4 +37,30 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 order, cancel
+
+    // === 연관관계 편의 메서드 ===
+    // 이 연관관계 메소드는 핵심적으로 컨트롤하는 쪽이 들고 있는게 좋다.
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+//    public static void main(String[] args) {
+//        Member member = new Member();
+//        Order order = new Order();
+//        member.getOrders().add(order);
+//        order.setMember(member);
+//    }
+    // 와 같이 쓰곤했는데 아래 두 줄을 하나로 묶는 것이 등장하게 되었다.
+    //
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
