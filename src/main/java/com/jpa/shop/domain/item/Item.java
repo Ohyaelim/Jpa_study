5dd4f6c08f045157ddc7131faf12f68f900debe1
Ohyaelim <npr05080@gmail.com>
2021-10-09 23:20:48 +0900

    feat: item entity 비지니스 로직 추가, 예외처리

--- src/main/java/com/jpa/shop/domain/item/Item.java
+++ src/main/java/com/jpa/shop/domain/item/Item.java
@@ -2,6 +2,7 @@ package com.jpa.shop.domain.item;
 //구현체를 가지고 할 것이므로 추상 클래스를 만든다.
 
 import com.jpa.shop.domain.Category;
+import com.jpa.shop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,5 +29,24 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //=== 비지니스 로직 ===//
+    // @Setter가 아니라 변경해야할 일이 있으면 이렇게 비지니스 로직을 짜서 핵심 메서드로 변경해주는 것이 맞다.
 
+    /**
+     * 제고 수량 stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 제고 수량 stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
