840a68a68a6198914f85cad54191339b50ccb5d8
Ohyaelim <npr05080@gmail.com>
2021-10-09 23:42:03 +0900

    feat: item service 구현

---
+++ src/main/java/com/jpa/shop/service/ItemService.java
@@ -0,0 +1,30 @@
+package com.jpa.shop.service;
+
+import com.jpa.shop.domain.item.Item;
+import com.jpa.shop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional // readonly는 저장이 안되니까
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
